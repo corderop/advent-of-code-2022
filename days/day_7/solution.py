54139d0746d0a4e927d045bf2c8e798f8d6b926f
Pablo Cordero <pablocorderoromero@gmail.com>
2022-12-13 20:22:47 +0100

    7: improve code

--- days/day_7/solution.py
+++ days/day_7/solution.py
@@ -1,10 +1,7 @@
 from utils import (
     calculate_folder_size,
     calculate_size_of_all_folders,
-    get_navigation_destination,
-    is_directory,
-    is_ls,
-    is_navigation,
+    process_commands_into_file_tree,
 )
 from file_tree import FileTree
 
@@ -13,20 +10,7 @@ def solution_1(input_text: str):
     MAX_FOLDER_SIZE = 100000
 
     lines = input_text.splitlines()
-    file_tree = FileTree()
-
-    for line in lines:
-        if is_directory(line) or is_ls(line):
-            continue
-
-        if is_navigation(line):
-            destination = get_navigation_destination(line)
-            file_tree.navigate_to(destination)
-            continue
-
-        file_size, file_name = line.split(" ")
-        file_tree.add_file_to_current_folder(file_name, int(file_size))
-
+    file_tree = process_commands_into_file_tree(lines)
     return calculate_size_of_all_folders(file_tree, MAX_FOLDER_SIZE)
 
 
@@ -35,19 +19,7 @@ def solution_2(input_text: str):
     NEEDED_SPACE = 30000000
 
     lines = input_text.splitlines()
-    file_tree = FileTree()
-
-    for line in lines:
-        if is_directory(line) or is_ls(line):
-            continue
-
-        if is_navigation(line):
-            destination = get_navigation_destination(line)
-            file_tree.navigate_to(destination)
-            continue
-
-        file_size, file_name = line.split(" ")
-        file_tree.add_file_to_current_folder(file_name, int(file_size))
+    file_tree = process_commands_into_file_tree(lines)
 
     folders = file_tree.get_all_folders()
     sizes = [calculate_folder_size(folder) for folder in folders]
--- days/day_7/utils.py
+++ days/day_7/utils.py
@@ -13,6 +13,24 @@ def calculate_size_of_all_folders(file_tree: FileTree, max_folder_size: int):
     return total_size
 
 
+def process_commands_into_file_tree(lines):
+    file_tree = FileTree()
+
+    for line in lines:
+        if is_directory(line) or is_ls(line):
+            continue
+
+        if is_navigation(line):
+            destination = get_navigation_destination(line)
+            file_tree.navigate_to(destination)
+            continue
+
+        file_size, file_name = line.split(" ")
+        file_tree.add_file_to_current_folder(file_name, int(file_size))
+
+    return file_tree
+
+
 def calculate_folder_size(folder):
     size = sum(folder.files.values())
 
